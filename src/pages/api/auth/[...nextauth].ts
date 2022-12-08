bc0826b52dfea40259d0f1cac5b93ce9a1016525
pertinate <ppertinate@gmail.com>
2022-12-07 23:52:08 -0600

    it's borked

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -9,10 +9,20 @@ import { prisma } from "../../../server/db/client";
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
-    session({ session, user }) {
+    // async jwt({ token, user, account, profile }) {
+    //   if (user) {
+    //     token.accessToken = account?.access_token;
+    //     token.id = user.id;
+    //   }
+    //   return token;
+    // },
+    async session({ session, token, user }) {
+      // session.accessToken = token.accessToken as string;
+
       if (session.user) {
-        session.user.id = user.id;
+        session.user.id = user.id as string;
       }
+
       return session;
     },
   },
@@ -22,9 +32,18 @@ export const authOptions: NextAuthOptions = {
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
+      token: "https://discord.com/api/oauth2/token",
+      authorization: { params: { scope: "identify" } },
     }),
     // ...add more providers here
   ],
+  // session: {
+  //   strategy: "jwt",
+  // },
+  // secret: process.env.NEXTAUTH_URL,
+  // jwt: {
+  //   secret: process.env.NEXTAUTH_JWT_SECRET,
+  // },
 };
 
 export default NextAuth(authOptions);
